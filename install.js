5dc7c3e15625ff4bf4d4a02a41f59e4eb727d25f
Nik <nik@niktek.io>
2023-05-08 10:15:21 +1000

    1683504921573

--- install.js
+++ install.js
@@ -2,18 +2,30 @@
 import { createSkeleton, SkeletonOptions } from 'create-skeleton-app';
 import { create } from 'create-svelte';
 import { spawnSync } from 'node:child_process';
+import { rmdirSync } from 'node:fs';
 
+spawnSync("git", ['switch', 'skeleton-bare'], { shell: true });
+try {
+	rmdirSync('skeleton-bare', { recursive: true });
+} catch (e) { }
 
-spawnSync("pnpm", ['up', '--latest'], {shell: true});
+spawnSync("pnpm", ['up', '--latest'], { shell: true });
 
 const so = new SkeletonOptions();
 so.packageManager = 'pnpm';
-so.name = 'skel-bare'
+so.name = 'skeleton-bare'
 // so.quiet = true;
 so.codeblocks = true;
 createSkeleton(so);
-
+spawnSync("git", ['add', '--all'], { shell: true });
+spawnSync("git", ['commit', '-m', Date.now().toString()], { shell: true });
+spawnSync("git", ['push'], { shell: true });
 // create-svelte
+spawnSync("git", ['switch', 'sveltekit-skeleton'], { shell: true });
+try {
+	rmdirSync('sveltekit-skeleton', { recursive: true });
+} catch (e) { }
+spawnSync("pnpm", ['up', '--latest'], { shell: true });
 const skopts = {
 	name: "",
 	template: 'skeleton',
@@ -23,4 +35,8 @@ const skopts = {
 	playwright: false,
 	vitest: true
 };
-create('sk-skeleton', skopts)
+create('sveltekit-skeleton', skopts)
+spawnSync("git", ['add', '--all'], { shell: true });
+spawnSync("git", ['commit', '-m', Date.now().toString()], { shell: true });
+spawnSync("git", ['push'], { shell: true });
+spawnSync("git", ['switch', 'main'], { shell: true });
